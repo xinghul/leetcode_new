7b96a320d65d17747a955c34dbdbd8a4aef6ccfe
Levi Lu <xinghu1989@gmail.com>
2017-12-11 20:52:02 -0800

    commit

--- 15_3Sum.java
+++ 15_3Sum.java
@@ -1,67 +1,46 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Map<Integer, List<List<Integer>>> twoSum = new HashMap<>();
+        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (nums.length < 3) {
+          return result;
+        }
 
         Arrays.sort(nums);
 
-        List<List<Integer>> result = new ArrayList<>();
-        Set<String> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++)
+        for (int last = 2; last < nums.length; last++)
         {
-          for (int j = 0; j < i; j++)
+          int low = 0, high = last - 1;
+
+          while (low < high)
           {
-            int sum = nums[i] + nums[j];
-            List<Integer> tmp = new ArrayList<>();
-            tmp.add(nums[j]);
-            tmp.add(nums[i]);
+            int sum = nums[low] + nums[high] + nums[last];
 
-            if (twoSum.containsKey(sum)) {
-              List<List<Integer>> list = twoSum.get(sum);
+            if (sum == 0) {
+              List<Integer> list = Arrays.asList(nums[low], nums[high], nums[last]);
+              set.add(list);
 
-              boolean exists = false;
-              for (List<Integer> sub: list)
+              while (low < nums.length - 1 && nums[low] == nums[low + 1])
               {
-                if (sub.get(0) == tmp.get(0) && sub.get(1) == tmp.get(1)) {
-                  exists = true;
-
-                  break;
-                }
+                low++;
+              }
+              while (high > 0 && nums[high] == nums[high - 1])
+              {
+                high--;
               }
 
-              if (!exists) {
-                list.add(tmp);
-              }
+              low++;
+              high--;
+            } else if (sum > 0) {
+              high--;
             } else {
-              List<List<Integer>> list = new ArrayList<>();
-
-              list.add(tmp);
-
-              twoSum.put(sum, list);
-            }
-          }
-
-          if (twoSum.containsKey(-nums[i])) {
-            List<List<Integer>> list = twoSum.get(-nums[i]);
-
-            for (List<Integer> tmp: list)
-            {
-              List<Integer> newList = new ArrayList(tmp);
-              newList.add(nums[i]);
-
-              String str = "";
-              for (int num: newList)
-              {
-                str += num + " ";
-              }
-
-              if (!set.contains(str)) {
-                result.add(newList);
-
-                set.add(str);
-              }
+              low++;
             }
           }
         }
+
+        result.addAll(set);
 
         return result;
     }
---
+++ 91_DecodeWays.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s.length() == 0) {
+          return 1;
+        }
+        
+        
+    }
+}
